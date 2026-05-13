74c1646fcb9df091481bf3f3d1fa5277d4b1d401
Sakib Hadziavdic <sakib@sake.ba>
2026-05-13 14:53:45 +0200

    Revert "deder-client-native v0.8.1"
    This reverts commit a3f88130de189e9bd05d03e22a18a0cec02a9cf3.

--- Formula/deder.rb
+++ Formula/deder.rb
@@ -1,22 +1,22 @@
-# Generated with JReleaser 1.24.0 at 2026-05-13T12:30:30.747189636Z
+# Generated with JReleaser 1.24.0 at 2026-05-13T11:35:57.109165307Z
 
 class Deder < Formula
   desc "Deder Build Tool"
   homepage "https://github.com/sake92/deder"
-  version "0.8.1"
+  version "0.8.0"
   license "Apache-2"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/sake92/deder/releases/download/v0.8.1/deder-client-linux-x86_64", :using => :nounzip
-    sha256 "b9e3036fe27d85387325ba71bb957a7a678f2b771501d9e14ab87a96b9f2b25f"
+    url "https://github.com/sake92/deder/releases/download/v0.8.0/deder-client-linux-x86_64", :using => :nounzip
+    sha256 "2742072e776f64c3c113e0bd1bcc97548544faff02b82f291e921f2085124d81"
 
     def install
       bin.install "deder-client-linux-x86_64" => "deder"
     end
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/sake92/deder/releases/download/v0.8.1/deder-client-osx-aarch_64", :using => :nounzip
-    sha256 "28fec756a0804ef85b1e8031a22510fa849bfc38215034ca0a1cb3a578251cd8"
+    url "https://github.com/sake92/deder/releases/download/v0.8.0/deder-client-osx-aarch_64", :using => :nounzip
+    sha256 "58037478ac612c503a62ae1fae122f98a580cf74198f2d8677cca75f63d4d0dc"
 
     def install
       bin.install "deder-client-osx-aarch_64" => "deder"
@@ -26,6 +26,6 @@ class Deder < Formula
 
   test do
     output = shell_output("#{bin}/deder --version")
-    assert_match "0.8.1", output
+    assert_match "0.8.0", output
   end
 end
